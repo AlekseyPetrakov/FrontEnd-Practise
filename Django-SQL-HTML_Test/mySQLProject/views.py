e3fe140e8891adcd84c92c14a3839f83924817b1
AlekseyPetrakov <119014994+AlekseyPetrakov@users.noreply.github.com>
2023-11-23 21:05:29 -0700

    Add files via upload

--- Django-SQL-HTML_Test/mySQLProject/views.py
+++ Django-SQL-HTML_Test/mySQLProject/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from .models import Person  # Adjust the import based on your actual models
+from .models import Person, Animal  # Adjust the import based on your actual models
+from django.http import HttpResponse
+
 
 def persons_list(request):
     persons_queryset = Person.objects.all()
-    return render(request, 'persons_list.html', {'persons': persons_queryset})
+    animals_queryset = Animal.objects.all()
+    return render(request, 'persons_list.html', {'persons': persons_queryset, 'animals': animals_queryset})
+
+
+
+def index(request):
+    person_exists = None
+
+    if request.method == 'POST':
+        fname = request.POST.get('fname')
+        person_exists = Person.objects.filter(name=fname).exists()
+
+    return render(request, 'index.html', {'person_exists': person_exists})
